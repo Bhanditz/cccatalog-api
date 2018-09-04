4a38b72727d35bba15302a9aea39ac7358d7c68e
Alden Page <aldenstpage@gmail.com>
2018-09-04 10:03:02 -0400

    Upgrade dev to 0.11.1

--- deployment/environments/dev/services/cccatalog-api/main.tf
+++ deployment/environments/dev/services/cccatalog-api/main.tf
@@ -31,8 +31,8 @@ module "cccatalog-api" {
   max_size                  = 3
   instance_type             = "t2.small"
   enable_monitoring         = false
-  git_revision              = "cb9e759846590463778a55823ee511dca0f4edbe"
-  api_version               = "0.11.0"
+  git_revision              = "f50c659745ede2055bd1b43d5a1d4314f371bed4"
+  api_version               = "0.11.1"
 
   # Environment-specific variables
   database_host             = "openledger-db-dev3-nvirginia.ctypbfibkuqv.us-east-1.rds.amazonaws.com"
